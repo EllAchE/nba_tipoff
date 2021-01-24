4bdec753ca889fe0b4afcb87f7d93a2de7ef22e3
logan-credera <lhar8771@gmail.com>
2021-01-24 02:50:11 -0500

    most base methods working

--- Data_Retrieval.py
+++ Data_Retrieval.py
@@ -26,7 +26,7 @@ Guy who compiled the tip off stats https://twitter.com/FattMemrite
 https://sportsbook.draftkings.com/leagues/basketball/103?category=game-props&subcategory=first-team-to-score
 '''
 
-# Request URL https://www.basketball-reference.com/boxscores/pbp/201901220OKC.html
+# URL for game https://www.basketball-reference.com/boxscores/pbp/201901220OKC.html
 # Where YYYYMMDD0### (# = home team code)
 
 # game schedule in order for seasons https://www.basketball-reference.com/leagues/NBA_2019_games.html
@@ -51,21 +51,19 @@ import re
 
 def get_games_played_links(start_season=2014, start_date=None):
 
-    #normal_months = ["october", "november", "december", "january", "february", "march", "april", "may", "june"]
-    #months_2020 = ["october-2019", "november", "december", "january", "february", "march", "july", "august", "september", "october-2020"]
-    #months_2021 = ["december", "january", "february", "march"] # may be a shortened season
-    start_season = 2021
+    # normal_months = ["october", "november", "december", "january", "february", "march", "april", "may", "june"]
+    # months_2020 = ["october-2019", "november", "december", "january", "february", "march", "july", "august", "september", "october-2020"]
+    # months_2021 = ["december", "january", "february", "march"] # may be a shortened season
     months = ["december"]
 
-    # regex -> csk="dddddddddaaa"
-
     seasons_list = list()
-    df_list = list()
     while start_season < 2022:
         seasons_list.append(str(start_season))
         start_season += 1
 
     sleep_counter = 0
+    game_list = list()
+    seasons_list = ["2019"]
     for season in seasons_list:
         # if season == 2020:
         #     months = months_2020
@@ -79,31 +77,92 @@ def get_games_played_links(start_season=2014, start_date=None):
             print("GET request for season", season, "month", month, "returned status", page.status_code)
             soup = BeautifulSoup(page.content, 'html.parser')
             sleep_counter = sleep_checker(sleep_counter)
-            table = soup.find_all('th', class_="left")
-            game_list = list()
-            for line in table:
-                game_str = str(line)
+            table_game_strs = soup.find_all('th', class_="left")
+            table_away_strs = soup.select('td[data-stat="visitor_team_name"]')
+            table_home_strs = soup.select('td[data-stat="home_team_name"]')
+            list_len = len(table_game_strs)
+            i = 0
+
+            while i < list_len - 1:
+                i += 1
+                game_str = str(table_game_strs[i])
+                away_str_full = str(table_away_strs[i].a.contents[0])
+                home_str_full = str(table_home_strs[i].a.contents[0])
+
                 s_index = game_str.index('csk="') + 5
+                # away_index = str(table_away_strs[i]).index('csk="') + 5
+                # home_index = str(table_home_strs[i]).index('csk="') + 5
+                away_str_short = str(table_away_strs[i])[s_index:s_index+3]
+                home_str_short = str(table_home_strs[i])[s_index:s_index+3]
+
                 # game_str = re.match(r'\csk='"\d\d\d\d\d\d\d\d\d\D\D\D\b", str(line)) #todo make this work with regex
-                game_list.append(game_str[s_index:s_index+12])
-                print(game_str[s_index:s_index+12])
-
-            # for game_link in soup.select_all('div'):
-            #     print(game_link)
-
-    pass
+                single_list = [game_str[s_index:s_index+12], home_str_full, away_str_full, home_str_short, away_str_short]
+                game_list.append(single_list)
+                # todo need to add home and away team pull here
+    return game_list
 
 
-def sleep_checker(sleep_counter):
+def sleep_checker(sleep_counter, base_time=2):
     sleep_counter += 1
     if sleep_counter % 4 == 0:
         print("sleeping for 2 + random seconds")
-        time.sleep(2 + random.random() * 3)
+        time.sleep(base_time + random.random() * 3)
         sleep_counter = 0
     return sleep_counter
 
-get_games_played_links()
 
-def generate_request_urls(games_data):
+def get_team_season_pairs(tag):
+    string = re.search(r'(?<=\"/teams/)(.*?)(?=\.)', str(tag.contents)).group(0)
+    team, season = string.split('/')
+    return [team, season]
 
+
+def get_player_team_in_season(player_link, season):
+    # https://www.basketball-reference.com/players/g/georgpa01.html
+    url = 'https://www.basketball-reference.com/{}'.format(player_link)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    all_team_matches_in_table = soup.select('td[data-stat="team_id"]')
+
+    team_season_pairs = list()
+    for tag in all_team_matches_in_table:
+        team_season_pairs.append(get_team_season_pairs(tag))
+
+    print()
+    # team_short = all_team_matches_in_table.map all_team_matches_in_table.contents.a.contents # Map this
+    # example of player with midseason trade James Harden
     pass
+
+
+def get_tipoff_winner_and_first_score(game_link, season, home_team, away_team):
+    # https://www.basketball-reference.com/boxscores/pbp/201901220OKC.html
+    url = 'https://www.basketball-reference.com/boxscores/pbp/{}.html'.format(game_link)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    possession_win_line = soup.select('td[colspan="5"]')[0].contents
+    first_score_line = soup.find_all('td', class_='bbr-play-score', limit=2)[1].contents  # second one
+
+    first_scoring_player = first_score_line[0].contents[0]
+    # link_index = str(first_score_line[0]).index('"')
+    first_scoring_player_link = re.search(r'(?<=")(.*?)(?=")', str(first_score_line[0])).group(0)
+    possession_gaining_player = possession_win_line[5].contents[0]
+    possession_gaining_player_link = re.search(r'(?<=")(.*?)(?=")', str(possession_win_line[5])).group(0)
+
+    home_tipper = possession_win_line[1].contents[0]
+    # home_tipper_link = re.search(r'(?<=")(.*?)(?=")', str(possession_win_line[1])).group(0)
+    away_tipper = possession_win_line[3].contents[0]
+    # away_tipper_link = re.search(r'(?<=")(.*?)(?=")', str(possession_win_line[3])).group(0)
+
+    possession_gaining_team = get_player_team_in_season(possession_gaining_player_link, season)
+    first_scoring_team = get_player_team_in_season(first_scoring_player_link, season)
+    home_wins = False
+    if possession_gaining_team == home_team:
+        home_wins = True
+    elif possession_gaining_player != away_team:
+        raise ValueError("player didn't match either team")
+
+    return home_tipper, away_tipper, first_scoring_player, possession_gaining_team, first_scoring_team, home_wins
+
+# get_tipoff_winner_and_first_score('201901220OKC', 2020, 'DAL', 'OKC')
+# get_games_played_links()
+get_player_team_in_season('/players/g/georgpa01.html', 2018)
