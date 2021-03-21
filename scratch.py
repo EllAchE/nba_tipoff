58d1aed2994ee1debf619a69f9bf7b64d06b0f45
EllAchE <lhar8771@gmail.com>
2021-03-20 22:57:00 -0500

    added meta summaries for all seasons since 2014

--- scratch.py
+++ scratch.py
@@ -2,19 +2,19 @@ from src.database.data_update import customDataUpdate, smallDataUpdate
 
 # customDataUpdate()
 from src.database.database_creation import addExtraUrlToPlayerLinks
-from src.historical_data.nba_play_by_play_methods import fillGaps
+from src.historical_data.nba_play_by_play_methods import fillGaps, teamSummaryDataFromFirstPointData
 from src.live_data.display_bets import getAllOddsAndDisplayByEv
 from src.live_data.live_odds_retrieval import betfairOdds, getDailyOdds
 from src.odds_and_statistics.prediction_enhancements import getFirstFieldGoalOrFirstPointStats
 
-getFirstFieldGoalOrFirstPointStats(2014)
-getFirstFieldGoalOrFirstPointStats(2015)
-getFirstFieldGoalOrFirstPointStats(2016)
-getFirstFieldGoalOrFirstPointStats(2017)
-getFirstFieldGoalOrFirstPointStats(2018)
-getFirstFieldGoalOrFirstPointStats(2019)
-getFirstFieldGoalOrFirstPointStats(2020)
-getFirstFieldGoalOrFirstPointStats(2021)
+teamSummaryDataFromFirstPointData(2014)
+teamSummaryDataFromFirstPointData(2015)
+teamSummaryDataFromFirstPointData(2016)
+teamSummaryDataFromFirstPointData(2017)
+teamSummaryDataFromFirstPointData(2018)
+teamSummaryDataFromFirstPointData(2019)
+teamSummaryDataFromFirstPointData(2020)
+teamSummaryDataFromFirstPointData(2021)
 
 # getDailyOdds('MEM', 'GSW', '-135')
 # smallDataUpdate()
