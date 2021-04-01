2ddd15d3dba08e0b749ea0f0dc4f1fff7e31e5eb
EllAchE <lhar8771@gmail.com>
2021-04-01 14:53:02 -0500

    data update

--- run_live_odds.py
+++ run_live_odds.py
@@ -1,9 +1,4 @@
 from src.live_data.display_bets import getAllOddsAndDisplayByEv, getUniqueOddsAndDisplayByEv
 
-# getAllOddsAndDisplayByEv(fanduelTomorrow=True)
-# getAllOddsAndDisplayByEv(bovada=True)
-# getAllOddsAndDisplayByEv(betfair=True)
+getAllOddsAndDisplayByEv(bovada=True, betfair=True)
 getAllOddsAndDisplayByEv(fanduelToday=True, mgm=True, draftkings=True, pointsbet=True, unibet=True, barstool=True, includeOptimalPlayerSpread=True)
-# getAllOddsAndDisplayByEv(getFanduelToday=True, getMgm=True) #, getPointsBet=True, getUnibet=True, getBarstool=True)
-# getAllOddsAndDisplayByEv(getDk=True)
-# getAllOddsAndDisplayByEv(getBovada=True)
